b7a2407272d90ead466e6790e10ce61cd2d1f9e5
Diony-G <dionygsr@gmail.com>
2026-03-21 23:49:57 -0400

    Add files via upload

---
+++ SistemTicket/Models/Entities/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using SistemTicket.Models.Enums;
+namespace SistemTicket.Models.Entities
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public TicketStatus Status { get; set; }
+
+        public TicketPriority? Priority { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
